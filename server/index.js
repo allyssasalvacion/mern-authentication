3ee00188992fe0235b99fcd4c3a3cb18344f3da5
Allyssa <asalbores7@gmail.com>
2024-02-08 20:42:35 +0800

    first commit

--- server/index.js
+++ server/index.js
@@ -5,13 +5,6 @@ const { mongoose } = require("mongoose");
 const cookieParser = require("cookie-parser");
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
 // database connection
 mongoose
   .connect(process.env.MONGO_URL)
